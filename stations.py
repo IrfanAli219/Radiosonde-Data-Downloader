902e3ec3d6e13c767c0e4dbfca55516ddced2645
IrfanAli219 <irfan14050@gmail.com>
2026-08-12 12:08:07 +0500

    A dictionary code for station to download data of

---
+++ stations.py
@@ -0,0 +1,40 @@
+STATIONS = {
+
+    # -------------------- PAKISTAN --------------------
+    # "Drosh": 41515,
+    # "Peshawar": 41530,
+    # "Muzaffarabad": 41532,
+    # "Parachinar": 41560,
+    # "Islamabad": 41571,
+    # "Murree": 41573,
+    # "Sargodha": 41594,
+    # "Jhelum": 41598,
+    # "Sialkot": 41600,
+    # "Zhob": 41620,
+    # "Dera_Ismail_Khan": 41624,
+    # "Lahore": 41640,
+    # "Quetta": 41660,
+    # "Multan": 41675,
+    # "Barkhan": 41685,
+    # "Kalat": 41696,
+    # "Nok_Kundi": 41710,
+    # "Dalbandin": 41712,
+    # "Jacobabad": 41715,
+    # "Khanpur": 41718,
+    # "Panjgur": 41739,
+    # "Khuzdar": 41744,
+    # "Nawabshah": 41749,
+    # "Jiwani": 41756,
+    # "Pasni": 41759,
+    # "Hyderabad": 41764,
+    # "Chhor": 41768,
+    # "Karachi": 41780,
+
+    # -------------------- INDIA (nearest to Pakistan border) --------------------
+    # "Amritsar": 42071,
+    # "Srinagar": 42027,
+    # "New_Delhi": 42182,
+    "Jammu": 42056,
+    # "Jodhpur": 42339,
+
+}
